02e52e6899ed96e5f8f153d6f72a5a70456ae216
ender orakci <reklamhesabim26@gmail.com>
2026-05-10 03:05:51 +0300

    PWA mobile app system added

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = "netsearch-cache-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/login.html",
+  "/register.html",
+  "/site.html",
+  "/admin.html",
+  "/user-panel.html",
+  "/settings.html",
+  "/manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
