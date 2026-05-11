cf12fb3356dff8a235ea3f27c5bc1a0c92b6211d
ender orakci <reklamhesabim26@gmail.com>
2026-05-11 16:21:43 +0300

    Mongo env adi duzeltildi

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGODB_URI || process.env.MONGO_URI)
 .then(() => {
     console.log("MongoDB bağlantısı başarılı");
 })
